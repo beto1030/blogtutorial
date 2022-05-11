b9d920302294c2868dbde871865d84134b3b261a
beto1030 <betoalvarado1030@gmail.com>
2022-05-11 00:57:57 -0700

    now using css grid instead of bootstrap grid flexbox

--- categories/index.php
+++ categories/index.php
@@ -9,52 +9,43 @@
         //print_r($results);
        
 ?>
-
-        <div class="container mt-4">
-                        <!--
-                        <div class="container m-5 p-5">
-                            <pre>
-                             <?php 
-                             //for($i = 0; $i<sizeof($results); $i++){
-                             //    if($results[$i]['categories']['title'] == $articles){
-                             //       echo "<p>".$results[$i]['categories']["title"]."</p>";
-                             //    }
-                             //}
-                             //print_r($results); 
-                             ?>
-                            </pre>
-                        </div>
-                        -->
-                        <div class="row">
-                            <?php for($i = 0; $i<sizeof($results); $i++){ 
+<div class="container mt-4" style="">
+                <div class="grid-container">
+                  <?php for($i = 0; $i<sizeof($results); $i++){ 
                                      if($results[$i]['categories']['title'] == $articles){?>
-                                         <a id="card" style="text-decoration: none;"class="col-sm-3" href="<?= $upFolderPlaceholder."post/index.php?post=".$results[$i]['slug']['current'];?>">
-                                             <div class="">
-                                                 <div class="card text-dark">
-                                                 <div class="card-header"><?= strtoupper($results[$i]['categories']["title"]); ?></div>
-                                                     <div class="card-body">
-                                                     <h5 class="card-title"><?= $results[$i]['title'] ?></h5>
-                                                     <p class="card-text">
-                                                         <?php
-                                                         $publishedAt_date = new DateTime($results[$i]["publishedAt"]);
-                                                         $updatedAt_date = new DateTime($results[$i]["_updatedAt"]);
-                                                         if($publishedAt_date < $updatedAt_date){
-                                                             echo "<small>".strtoupper($updatedAt_date->format('M j, Y'))."</small>";
-                                                         }else {
-                                                             echo "<small>".strtoupper($publishedAt_date->format('M j, Y'))."</small>";
-                                                         }
-                                                         ?>
-                                                     </p>
-                                                     </div>
-                                                 </div>
-                                             </div>    
-                                         </a>
-                            <?php 
-                                     }
-                                  } 
-                            ?>
-                        </div>
-        </div>
+                            <div class="grid-item" style="height: 90%;">
+                                <a id="card" style="text-decoration: none;"class="col-sm-3" href="<?= $upFolderPlaceholder."post/index.php?post=".$results[$i]['slug']['current'];?>">
+                                    <div class="card h-100 text-dark">
+                                           <div class="card-header" style="padding-left: 20px;"><?= strtoupper($results[$i]['categories']["title"]); ?></div>
+                                           <div class="card-body d-flex flex-column" style="padding: 0px 20px 0px;">
+                                               <h5 class="card-title"><?= $results[$i]['title'] ?></h5>
+                                               <p class="card-text ellipsis"><?= $results[$i]['summary'] ?></p>
+                                           </div>
+                                           <div class="card-footer" style="border: none; background-color: #fff; margin-left: 5px; padding-top: 0;">
+                                               <p class="card-text text-muted" style="">
+                                                   <?php
+                                                   $publishedAt_date = new DateTime($results[$i]["publishedAt"]);
+                                                   $updatedAt_date = new DateTime($results[$i]["_updatedAt"]);
+                                                   if($publishedAt_date < $updatedAt_date){
+                                                       echo "<small>".strtoupper($updatedAt_date->format('M j, Y'))."</small>";
+                                                   }else {
+                                                       echo "<small>".strtoupper($publishedAt_date->format('M j, Y'))."</small>";
+                                                   }
+                                                   ?>
+                                               </p>
+                                           </div>
+
+                                           
+                                    </div>
+                                </a>
+                            </div>
+                  <?php }
+                } ?>
+                </div>
+                
+</div>
+
+        
 <?php } ?>
 
 <?php include_once("../templates/bottom.php"); ?>
